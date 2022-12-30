a06323347b419bf6349ec832ed328be028d5ecc0
George <georgemishchenko@gmail.com>
2022-12-30 23:24:52 +0100

    Added feedback form

--- public/js/feedbacks.js
+++ public/js/feedbacks.js
@@ -1,6 +1,10 @@
 window.onload = () => {
     const optionsBtn = document.getElementById('options-btn')
     const panel = document.getElementsByClassName('panel')[0]
+    const submitButton = document.getElementById('submit')
+    submitButton.disabled = true
+    submitButton.style.background = '#40d276'
+
 
     optionsBtn.onclick = () => {
         if (optionsBtn.textContent === '🠈') {
@@ -13,4 +17,22 @@ window.onload = () => {
             optionsBtn.style.background = '#f73123'
         }
     }
+
+    const rating = document.getElementById('rating')
+    const stars = document.getElementsByClassName('stars')[0]
+
+    rating.value = 0
+
+    rating.addEventListener('change', () => {
+        let text = ''
+
+        for (let i = 0; i < 5; i++) {      
+            if (i < rating.value) text += '★' 
+            else text += '☆'  
+        }  
+        
+        stars.textContent = text
+        submitButton.disabled = false
+        submitButton.style.background = '#1aa34c'
+    })
 }
